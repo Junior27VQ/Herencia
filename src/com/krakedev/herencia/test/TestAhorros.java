1a67e8b4d1dfd89885055b8901c24cb66dd635f1
Junior27VQ <favq1999@outlook.com>
2026-04-27 18:01:44 -0500

    FAVQ: add savings test

--- src/com/krakedev/herencia/test/TestAhorros.java
+++ src/com/krakedev/herencia/test/TestAhorros.java
@@ -1,9 +1,28 @@
 package com.krakedev.herencia.test;
 
+import com.krakedev.herencia.Hija;
+import com.krakedev.herencia.Hijo;
+import com.krakedev.herencia.Padre;
 
 public class TestAhorros {
 
 	public static void main(String[] args) {
+		Padre p=new Padre();
+		p.ahorrar(125);
+		p.setDefectos(6);
+		p.setVirtudes(7);
+		System.out.println(p);
+		
+		Hija hija=new Hija();
+		hija.ahorrar(125);
+		hija.setDefectos(4);
+		hija.setVirtudes(5);
+		System.out.println(hija);
+		
+		Hijo hijo=new Hijo(1,2,3);
+		hijo.ahorrar(125);
+		System.out.println(hijo);
+
 
 	}
 
