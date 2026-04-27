d5f5dd8cfe2c673268592356247f62d5eee9d3e4
Junior27VQ <favq1999@outlook.com>
2026-04-27 17:47:36 -0500

    FAVQ: add child class

--- src/com/krakedev/herencia/Hijo.java
+++ src/com/krakedev/herencia/Hijo.java
@@ -1,7 +1,18 @@
 package com.krakedev.herencia;
 
 public class Hijo extends Padre{
-	
+	private int jugetes;
+
+	public Hijo(int virtudes, int defectos, int jugetes) {
+		super(defectos, virtudes);
+		this.jugetes = jugetes;
+	}
+
+	@Override
+	public String toString() {
+		return "Hijo [defectos= " + getDefectos() + ", virtudes=" + getVirtudes() + "[jugetes=" + jugetes +"]";
+	}
+
 	
 
 }
